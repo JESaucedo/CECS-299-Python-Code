d0c9881550a8529201d349179f8e467f32d2725c
JESaucedo <60938062+JESaucedo@users.noreply.github.com>
2020-03-07 18:38:26 -0800

    Problem #2 completed
    Finished the encryption for a shift and affine cipher

--- hw2.py
+++ hw2.py
@@ -70,3 +70,73 @@ def modinv(a,m):
         result = result + m
         
     return result
+
+def letters2digits(letters):
+    """converts a string of letters A-Z to a string of double digits without spaces in the range 00-25"""
+    letter2digit = {"A" : "00", "B" : "01", "C" : "02", "D" : "03", "E" : "04", 
+                  "F" : "05", "G" : "06", "H" : "07", "I" : "08", "J" : "09",
+                  "K" : "10", "L" : "11", "M" : "12", "N" : "13", "O" : "14",  
+                  "P" : "15", "Q" : "16", "R" : "17", "S" : "18", "T" : "19",
+                  "U" : "20", "V" : "21", "W" : "22", "X" : "23", "Y" : "24", 
+                  "Z" : "25"}
+        
+    digits = ""  #initializing digits string
+    letters = "".join(letters.split()) #removing whitespaces in the text
+
+    
+    for i in range(0, len(letters)):
+        if(letters[i].isalpha()):
+            letter = letters[i].upper()  #converting to uppercase
+            digit = letter2digit[letter]
+            digits += digit     # concatenating to the string of digits
+    
+    return digits
+
+def digits2letters(digits):
+    """converts a string of double digits without spaces in the range 00-25 to a string of letters A-Z"""
+    letter2digit = {"A" : "00", "B" : "01", "C" : "02", "D" : "03", "E" : "04", 
+                  "F" : "05", "G" : "06", "H" : "07", "I" : "08", "J" : "09",
+                  "K" : "10", "L" : "11", "M" : "12", "N" : "13", "O" : "14",  
+                  "P" : "15", "Q" : "16", "R" : "17", "S" : "18", "T" : "19",
+                  "U" : "20", "V" : "21", "W" : "22", "X" : "23", "Y" : "24", 
+                  "Z" : "25"}
+        
+    digit2letter = dict((v,k) for k,v in letter2digit.items())  #creating a dictionary with keys and values exchanged
+        
+    letters = ""
+    start = 0  #initializing starting index of first digit
+    for i in range(0, len(digits), 2):
+        digit = digits[start : start + 2]  # accessing the double digit
+        letters += digit2letter[digit]     # concatenating to the string of letters
+        start += 2                         # updating the starting index for next digit
+    
+    return letters
+
+#Problem 2
+def affineEncrypt(text, a, b):
+    """encrypts the plaintext 'text', using an affine transformation key (a, b)
+    INPUT:  text - plaintext as a string of letters
+            a - integer satisfying gcd(a, 26) = 1.  Raises error if such is not the case
+            b - integer 
+            
+    OUTPUT: The encrypted message as a string of characters
+    """
+    result = gcd(a,26)
+    if(result != 1):
+        raise ValueError("The given key is invalid. The gcd(a,26) must be 1")
+    encodedNumbers = letters2digits(text)
+    print(encodedNumbers)
+    encryptedNumber = ""
+    start = 0  #initializing starting index of first digit
+    for i in range(0, len(encodedNumbers), 2):
+        digit = encodedNumbers[start : start + 2]  # accessing the double digit
+       
+        newEncryptedNumber = str((a*int(digit) + b) % 26) 
+        #this is the function of converting the message into an encoded message and had to cast digit as an integer to do arithmetic and then cast as string to concantanate
+        if(int(newEncryptedNumber) < 10):
+            newEncryptedNumber = "0" + newEncryptedNumber #this is to make sure that it remains double digits so it can be used in the function digits2letters method
+        #print(newEncryptedNumber)
+        encryptedNumber += str(newEncryptedNumber)
+        start += 2
+    encryptedMessage = digits2letters(encryptedNumber)
+    return encryptedMessage
